248476b78befb757eeaabe97a33239bf6da4d037
Alukon <111476972+Alukon@users.noreply.github.com>
2024-03-09 16:13:37 +1000

    randomColor

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const cols = document.querySelectorAll('.col');
+
+//function generateRandomColor(){
+//const hexCodes = '0123456789ABCDEF'
+//let color = '';
+//for (let i=0; i<6; i++){
+//    color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+//    }
+//return '#' + color
+//}
+
+document.addEventListener('keydown', (event) => {
+//    console.log(event.code);
+    event.preventDefault()
+    if (event.code.toLowerCase() == 'space'){
+        setRandomColors()
+        }
+})
+document.addEventListener('click', (event) =>{
+    const type = event.target.dataset.type;
+    if (type == 'lock'){
+        const node = event.target.tagName.toLowerCase() == 'i' ? event.target : event.target.children[0]
+
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
+    }
+})
+
+function setRandomColors(){
+    cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
+        const text = col.querySelector('h2');
+        const button = col.querySelector('button');
+        const color = chroma.random();
+
+        if(isLocked){
+            return
+        };
+
+        text.textContent = color;
+        col.style.background = color;
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+};
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+setRandomColors();
